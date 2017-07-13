7c31fe77e2e7f66f3e98c847f96be8c4c57ca620
tcatrain <thierrycatrain@gmail.com>
2017-07-13 13:32:34 +0000

    Fixing error introduced in @chibiketan pull request regarding non VS build

--- HoMM3-Lod/Include/Types.hpp
+++ HoMM3-Lod/Include/Types.hpp
@@ -4,8 +4,12 @@ typedef unsigned char byte;
 typedef unsigned short int usint;
 typedef unsigned int uint;
 
-#ifdef HoMM3_Lod_EXPORTS
-#  define HOMM3_LOD_LIB __declspec(dllexport)
+#ifdef COMPILER_MSVC
+    #ifdef HoMM3_Lod_EXPORTS
+    #  define HOMM3_LOD_LIB __declspec(dllexport)
+    #else
+    #  define HOMM3_LOD_LIB __declspec(dllimport)
+    #endif
 #else
-#  define HOMM3_LOD_LIB 
-#endif // HoMM3_Lod_EXPORTS
+    # define HOMM3_LOD_LIB
+#endif
