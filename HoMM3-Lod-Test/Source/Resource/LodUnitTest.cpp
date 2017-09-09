8688164f687a8dd74735a79f931e89bba1d6e2f7
tcatrain <thierrycatrain@gmail.com>
2017-09-09 18:41:52 +0200

    Testing when the resource is not loaded

--- HoMM3-Lod-Test/Source/Resource/LodUnitTest.cpp
+++ HoMM3-Lod-Test/Source/Resource/LodUnitTest.cpp
@@ -4,8 +4,7 @@
 class LodUnitTest : public ::testing::Test
 {
 public:
-    HoMM3::Resource::Lod* filelod_;
-    HoMM3::Resource::Lod* veclod_;
+    std::vector<byte> bytes_;
 
     virtual void SetUp()
     {
@@ -13,56 +12,71 @@ public:
         ifs.seekg(0, std::ios::end);
         uint length = ifs.tellg();
         ifs.seekg(0, std::ios::beg);
-        std::vector<byte> bytes(length);
-        ifs.read(reinterpret_cast<char*>(&bytes[0]), length);
-        
-        this->filelod_ = new HoMM3::Resource::Lod("txt_content.lod");
-        this->veclod_ = new HoMM3::Resource::Lod(bytes);
+        this->bytes_.resize(length);
+        ifs.read(reinterpret_cast<char*>(&this->bytes_[0]), length);
     }
 
     virtual void TearDown()
     {
-        delete this->filelod_;
-        delete this->veclod_;
     }
 };
 
 TEST_F(LodUnitTest, LodIsProperlyLoadedFromFile)
 {
-    ASSERT_EQ((uint) 0x444F4C, this->filelod_->Header().key);
-    ASSERT_EQ((uint) 500, this->filelod_->Header().type);
-    ASSERT_EQ((uint) 74, this->filelod_->Header().nb);
-    ASSERT_EQ((uint) 74, this->filelod_->EntriesHeaders().size());
+    HoMM3::Resource::Lod lod("txt_content.lod");
+    
+    ASSERT_EQ(true, lod.IsLoaded());
+    ASSERT_EQ((uint) 0x444F4C, lod.Header().key);
+    ASSERT_EQ((uint) 500, lod.Header().type);
+    ASSERT_EQ((uint) 74, lod.Header().nb);
+    ASSERT_EQ((uint) 74, lod.EntriesHeaders().size());
 
-    ASSERT_STREQ("AdvEvent.txt", (const char*) this->filelod_->EntriesHeaders()[0].get()->name);
-    ASSERT_EQ((uint) 2, this->filelod_->EntriesHeaders()[0].get()->type);
-    ASSERT_EQ((uint) 1084872, this->filelod_->EntriesHeaders()[0].get()->offset);
-    ASSERT_EQ((uint) 25344, this->filelod_->EntriesHeaders()[0].get()->size);
-    ASSERT_EQ((uint) 8059, this->filelod_->EntriesHeaders()[0].get()->zsize);
+    ASSERT_STREQ("AdvEvent.txt", (const char*) lod.EntriesHeaders()[0].get()->name);
+    ASSERT_EQ((uint) 2, lod.EntriesHeaders()[0].get()->type);
+    ASSERT_EQ((uint) 1084872, lod.EntriesHeaders()[0].get()->offset);
+    ASSERT_EQ((uint) 25344, lod.EntriesHeaders()[0].get()->size);
+    ASSERT_EQ((uint) 8059, lod.EntriesHeaders()[0].get()->zsize);
 
-    ASSERT_STREQ("XtraInfo.txt", (const char*) this->filelod_->EntriesHeaders()[73].get()->name);
-    ASSERT_EQ((uint) 2, this->filelod_->EntriesHeaders()[73].get()->type);
-    ASSERT_EQ((uint) 1193109, this->filelod_->EntriesHeaders()[73].get()->offset);
-    ASSERT_EQ((uint) 1273, this->filelod_->EntriesHeaders()[73].get()->size);
-    ASSERT_EQ((uint) 357, this->filelod_->EntriesHeaders()[73].get()->zsize);
+    ASSERT_STREQ("XtraInfo.txt", (const char*) lod.EntriesHeaders()[73].get()->name);
+    ASSERT_EQ((uint) 2, lod.EntriesHeaders()[73].get()->type);
+    ASSERT_EQ((uint) 1193109, lod.EntriesHeaders()[73].get()->offset);
+    ASSERT_EQ((uint) 1273, lod.EntriesHeaders()[73].get()->size);
+    ASSERT_EQ((uint) 357, lod.EntriesHeaders()[73].get()->zsize);
 }
 
+TEST_F(LodUnitTest, LodIsNotLoadedFromMissingFile)
+{
+    HoMM3::Resource::Lod lod("missing.lod");
+    
+    ASSERT_EQ(false, lod.IsLoaded());
+}
+    
 TEST_F(LodUnitTest, LodIsProperlyLoadedFromVector)
 {
-    ASSERT_EQ((uint) 0x444F4C, this->veclod_->Header().key);
-    ASSERT_EQ((uint) 500, this->veclod_->Header().type);
-    ASSERT_EQ((uint) 74, this->veclod_->Header().nb);
-    ASSERT_EQ((uint) 74, this->veclod_->EntriesHeaders().size());
+    HoMM3::Resource::Lod lod(this->bytes_);
     
-    ASSERT_STREQ("AdvEvent.txt", (const char*) this->veclod_->EntriesHeaders()[0].get()->name);
-    ASSERT_EQ((uint) 2, this->veclod_->EntriesHeaders()[0].get()->type);
-    ASSERT_EQ((uint) 1084872, this->veclod_->EntriesHeaders()[0].get()->offset);
-    ASSERT_EQ((uint) 25344, this->veclod_->EntriesHeaders()[0].get()->size);
-    ASSERT_EQ((uint) 8059, this->veclod_->EntriesHeaders()[0].get()->zsize);
+    ASSERT_EQ(true, lod.IsLoaded());
+    ASSERT_EQ((uint) 0x444F4C, lod.Header().key);
+    ASSERT_EQ((uint) 500, lod.Header().type);
+    ASSERT_EQ((uint) 74, lod.Header().nb);
+    ASSERT_EQ((uint) 74, lod.EntriesHeaders().size());
     
-    ASSERT_STREQ("XtraInfo.txt", (const char*) this->veclod_->EntriesHeaders()[73].get()->name);
-    ASSERT_EQ((uint) 2, this->veclod_->EntriesHeaders()[73].get()->type);
-    ASSERT_EQ((uint) 1193109, this->veclod_->EntriesHeaders()[73].get()->offset);
-    ASSERT_EQ((uint) 1273, this->veclod_->EntriesHeaders()[73].get()->size);
-    ASSERT_EQ((uint) 357, this->veclod_->EntriesHeaders()[73].get()->zsize);
+    ASSERT_STREQ("AdvEvent.txt", (const char*) lod.EntriesHeaders()[0].get()->name);
+    ASSERT_EQ((uint) 2, lod.EntriesHeaders()[0].get()->type);
+    ASSERT_EQ((uint) 1084872, lod.EntriesHeaders()[0].get()->offset);
+    ASSERT_EQ((uint) 25344, lod.EntriesHeaders()[0].get()->size);
+    ASSERT_EQ((uint) 8059, lod.EntriesHeaders()[0].get()->zsize);
+    
+    ASSERT_STREQ("XtraInfo.txt", (const char*) lod.EntriesHeaders()[73].get()->name);
+    ASSERT_EQ((uint) 2, lod.EntriesHeaders()[73].get()->type);
+    ASSERT_EQ((uint) 1193109, lod.EntriesHeaders()[73].get()->offset);
+    ASSERT_EQ((uint) 1273, lod.EntriesHeaders()[73].get()->size);
+    ASSERT_EQ((uint) 357, lod.EntriesHeaders()[73].get()->zsize);
+}
+
+TEST_F(LodUnitTest, LodIsNotLoadedFromEmptyVector)
+{
+    HoMM3::Resource::Lod lod((std::vector<byte>()));
+    
+    ASSERT_EQ(false, lod.IsLoaded());
 }
--- HoMM3-Lod-Test/Source/Resource/PcxUnitTest.cpp
+++ HoMM3-Lod-Test/Source/Resource/PcxUnitTest.cpp
@@ -4,49 +4,64 @@
 class PcxUnitTest : public ::testing::Test
 {
 public:
-    HoMM3::Resource::Pcx* filepcx_;
-    HoMM3::Resource::Pcx* vecpcx_;
-
+    std::vector<byte> bytes_;
+    
     virtual void SetUp()
     {
         std::ifstream ifs("ab01_01.pcx", std::ios::binary);
         ifs.seekg(0, std::ios::end);
         uint length = ifs.tellg();
         ifs.seekg(0, std::ios::beg);
-        std::vector<byte> bytes(length);
-        ifs.read(reinterpret_cast<char*>(&bytes[0]), length);
-
-        this->filepcx_ = new HoMM3::Resource::Pcx("ab01_01.pcx");
-        this->vecpcx_ = new HoMM3::Resource::Pcx(bytes);
+        this->bytes_.resize(length);
+        ifs.read(reinterpret_cast<char*>(&this->bytes_[0]), length);
     }
 
     virtual void TearDown()
     {
-        delete this->filepcx_;
-        delete this->vecpcx_;
     }
 };
 
 TEST_F(PcxUnitTest, PcxIsProperlyLoadedFromFile)
 {
-    ASSERT_EQ((uint) 1692, this->filepcx_->Header().size);
-    ASSERT_EQ((uint) 3, this->filepcx_->Header().type);
-    ASSERT_EQ((uint) 96, this->filepcx_->Header().flwidth);
-    ASSERT_EQ((uint) 64, this->filepcx_->Header().flheight);
-    ASSERT_EQ((uint) 96, this->filepcx_->Header().fmwidth);
-    ASSERT_EQ((uint) 55, this->filepcx_->Header().fmheight);
-    ASSERT_EQ((uint) 0, this->filepcx_->Header().xmargin);
-    ASSERT_EQ((uint) 5, this->filepcx_->Header().ymargin);
+    HoMM3::Resource::Pcx pcx("ab01_01.pcx");
+    
+    ASSERT_EQ(true, pcx.IsLoaded());
+    ASSERT_EQ((uint) 1692, pcx.Header().size);
+    ASSERT_EQ((uint) 3, pcx.Header().type);
+    ASSERT_EQ((uint) 96, pcx.Header().flwidth);
+    ASSERT_EQ((uint) 64, pcx.Header().flheight);
+    ASSERT_EQ((uint) 96, pcx.Header().fmwidth);
+    ASSERT_EQ((uint) 55, pcx.Header().fmheight);
+    ASSERT_EQ((uint) 0, pcx.Header().xmargin);
+    ASSERT_EQ((uint) 5, pcx.Header().ymargin);
 }
+
+TEST_F(PcxUnitTest, PcxIsNotLoadedFromMissingFile)
+{
+    HoMM3::Resource::Pcx pcx("missing.pcx");
+    
+    ASSERT_EQ(false, pcx.IsLoaded());
+}
+
 
 TEST_F(PcxUnitTest, PcxIsProperlyLoadedFromVector)
 {
-    ASSERT_EQ((uint) 1692, this->vecpcx_->Header().size);
-    ASSERT_EQ((uint) 3, this->vecpcx_->Header().type);
-    ASSERT_EQ((uint) 96, this->vecpcx_->Header().flwidth);
-    ASSERT_EQ((uint) 64, this->vecpcx_->Header().flheight);
-    ASSERT_EQ((uint) 96, this->vecpcx_->Header().fmwidth);
-    ASSERT_EQ((uint) 55, this->vecpcx_->Header().fmheight);
-    ASSERT_EQ((uint) 0, this->vecpcx_->Header().xmargin);
-    ASSERT_EQ((uint) 5, this->vecpcx_->Header().ymargin);
+    HoMM3::Resource::Pcx pcx(this->bytes_);
+    
+    ASSERT_EQ(true, pcx.IsLoaded());
+    ASSERT_EQ((uint) 1692, pcx.Header().size);
+    ASSERT_EQ((uint) 3, pcx.Header().type);
+    ASSERT_EQ((uint) 96, pcx.Header().flwidth);
+    ASSERT_EQ((uint) 64, pcx.Header().flheight);
+    ASSERT_EQ((uint) 96, pcx.Header().fmwidth);
+    ASSERT_EQ((uint) 55, pcx.Header().fmheight);
+    ASSERT_EQ((uint) 0, pcx.Header().xmargin);
+    ASSERT_EQ((uint) 5, pcx.Header().ymargin);
+}
+
+TEST_F(PcxUnitTest, PcxIsNotLoadedFromEmptyVector)
+{
+    HoMM3::Resource::Pcx pcx((std::vector<byte>()));
+    
+    ASSERT_EQ(false, pcx.IsLoaded());
 }
