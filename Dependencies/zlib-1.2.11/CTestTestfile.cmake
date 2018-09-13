8cfcf0c8d0d31fa9122e7581ebb00303f1b1362c
tcatrain <thierrycatrain@gmail.com>
2018-09-13 09:38:30 +0200

    Updating versions of zlib from 2.8 to 2.11 and googletest

---
+++ Dependencies/zlib-1.2.11/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/thierry/Projets/HoMM3/Dependencies/zlib-1.2.11
+# Build directory: /Users/thierry/Projets/HoMM3/Dependencies/zlib-1.2.11
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
